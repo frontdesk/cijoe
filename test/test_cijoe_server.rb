9c974025b04e06686a0ebc5e5ad68ea08b9b4ab9
Nikolai Vladimirov <nikolay@vladimiroff.com>
2012-07-29 11:44:08 +0300

    stub CIJoe in the server tests
    app.joe is actually not accessable. And it shouldn't be.

--- test/test_cijoe_server.rb
+++ test/test_cijoe_server.rb
@@ -6,38 +6,33 @@ class TestCIJoeServer < Test::Unit::TestCase
   include Rack::Test::Methods
 
   class ::CIJoe
-    attr_writer :current_build, :last_build
-  end
-
-  attr_accessor :app
-
-  def setup
-    @app = CIJoe::Server.new
-    joe = @app.joe
-    
     # make Build#restore a no-op so we don't overwrite our current/last
     # build attributes set from tests.
-    def joe.restore
+    def restore
     end
-    
+
     # make CIJoe#build! and CIJoe#git_update a no-op so we don't overwrite our local changes
     # or local commits nor should we run tests.
-    def joe.build!
+    def build!
     end
+  end
+
+  def app
+    CIJoe::Server.new
   end
 
   def test_ping
-    app.joe.last_build = build :worked
-    assert !app.joe.building?, "have a last build, but not a current"
+    last_build = build :worked
+    CIJoe.any_instance.stubs(:last_build).returns(last_build)
 
     get "/ping"
     assert_equal 200, last_response.status
-    assert_equal app.joe.last_build.sha, last_response.body
+    assert_equal last_build.sha, last_response.body
   end
 
   def test_ping_building
-    app.joe.current_build = build :building
-    assert app.joe.building?, "buildin' a awsum project"
+    current_build = build :building
+    CIJoe.any_instance.stubs(:current_build).returns(current_build)
 
     get "/ping"
     assert_equal 412, last_response.status
@@ -45,9 +40,12 @@ class TestCIJoeServer < Test::Unit::TestCase
   end
 
   def test_ping_building_with_a_previous_build
-    app.joe.last_build = build :worked
-    app.joe.current_build = build :building
-    assert app.joe.building?, "buildin' a awsum project"
+    last_build = build :worked
+    CIJoe.any_instance.stubs(:last_build).returns(last_build)
+
+    current_build = build :building
+    CIJoe.any_instance.stubs(:current_build).returns(current_build)
+    CIJoe.any_instance.stubs(:building?).returns(true)
 
     get "/ping"
     assert_equal 412, last_response.status
@@ -55,61 +53,63 @@ class TestCIJoeServer < Test::Unit::TestCase
   end
 
   def test_ping_failed
-    app.joe.last_build = build :failed
+    last_build = build :failed
+    CIJoe.any_instance.stubs(:last_build).returns(last_build)
 
     get "/ping"
     assert_equal 412, last_response.status
-    assert_equal app.joe.last_build.sha, last_response.body
-  end
-
-  def test_ping_should_not_reset_current_build_in_tests
-    current_build = build :building
-    app.joe.current_build = current_build
-    assert app.joe.building?
-    get "/ping"
-    assert_equal current_build, app.joe.current_build
+    assert_equal last_build.sha, last_response.body
   end
 
   def test_post_with_json_works
+    CIJoe.any_instance.expects(:build)
+
     post '/', :payload => File.read("#{Dir.pwd}/test/fixtures/payload.json")
-    assert app.joe.building?
-    assert_equal 302, last_response.status
-  end
-  
-  def test_post_does_not_build_on_branch_mismatch
-    post "/", :payload => {"ref" => "refs/heads/dont_build"}.to_json
-    assert !app.joe.building?
     assert_equal 302, last_response.status
   end
 
-  def test_post_builds_specific_branch 
-    app.joe.expects(:build!).with("branchname")
+  def test_post_does_not_build_on_branch_mismatch
+    CIJoe.any_instance.expects(:build).never
+
+    post "/", :payload => {"ref" => "refs/heads/dont_build"}.to_json
+
+    assert_equal 302, last_response.status
+  end
+
+  def test_post_builds_specific_branch
+    CIJoe.any_instance.expects(:build).with("branchname")
+
     post "/?branch=branchname", :payload => {"ref" => "refs/heads/master"}.to_json
-    assert app.joe.building?
     assert_equal 302, last_response.status
   end
 
   def test_post_does_build_on_branch_match
+    CIJoe.any_instance.expects(:build)
+
     post "/", :payload => {"ref" => "refs/heads/master"}.to_json
-    assert app.joe.building?
     assert_equal 302, last_response.status
   end
-  
+
   def test_post_does_build_when_build_button_is_used
+    CIJoe.any_instance.expects(:build)
+
     post "/", :rebuild => true
-    assert app.joe.building?
     assert_equal 302, last_response.status
   end
 
   def test_jsonp_should_return_plain_json_without_param
-    app.joe.last_build = build :failed
+    last_build = build :failed
+    CIJoe.any_instance.stubs(:last_build).returns(last_build)
+
     get "/api/json"
     assert_equal 200, last_response.status
     assert_equal 'application/json', last_response.content_type
   end
 
   def test_jsonp_should_return_jsonp_with_param
-    app.joe.last_build = build :failed
+    last_build = build :failed
+    CIJoe.any_instance.stubs(:last_build).returns(last_build)
+
     get "/api/json?jsonp=fooberz"
     assert_equal 200, last_response.status
     assert_equal 'application/json', last_response.content_type
