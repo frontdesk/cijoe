a34e2fe3211697b5900879a852d28aa14da26b84
Nikolai Vladimirov <nikolay@vladimiroff.com>
2012-09-22 16:21:39 +0300

    rewrite server test as spec

--- test/test_cijoe_server.rb
+++ test/test_cijoe_server.rb
@@ -2,7 +2,7 @@ require "helper"
 require "rack/test"
 require "cijoe/server"
 
-class TestCIJoeServer < MiniTest::Unit::TestCase
+describe CIJoe::Server do
   include Rack::Test::Methods
 
   class ::CIJoe
@@ -21,118 +21,133 @@ class TestCIJoeServer < MiniTest::Unit::TestCase
     CIJoe::Server.new
   end
 
-  def test_ping
-    last_build = build :worked
-    CIJoe.any_instance.stubs(:last_build).returns(last_build)
+  describe 'GET /ping' do
+    it 'should work with no build' do
+      get "/ping"
 
-    get "/ping"
-    assert_equal 200, last_response.status
-    assert_equal last_build.sha, last_response.body
+      last_response.status.must_equal 412
+    end
+
+    it 'responds with the last build sha if the build was ok' do
+      last_build = build :worked
+      CIJoe.any_instance.stubs(:last_build).returns(last_build)
+
+      get "/ping"
+
+      last_response.status.must_equal 200
+      last_response.body.must_equal last_build.sha
+    end
+
+    it 'responds with building if the build is in progress' do
+      current_build = build :building
+      CIJoe.any_instance.stubs(:current_build).returns(current_build)
+
+      get "/ping"
+
+      last_response.status.must_equal 412
+      last_response.body.must_equal 'building'
+    end
+
+    it 'uses the current build status' do
+      last_build = build :worked
+      CIJoe.any_instance.stubs(:last_build).returns(last_build)
+
+      current_build = build :building
+      CIJoe.any_instance.stubs(:current_build).returns(current_build)
+      CIJoe.any_instance.stubs(:building?).returns(true)
+
+      get "/ping"
+
+      last_response.status.must_equal 412
+      last_response.body.must_equal 'building'
+    end
+
+    it 'responds with non success response if the build failed' do
+      last_build = build :failed
+      CIJoe.any_instance.stubs(:last_build).returns(last_build)
+
+      get "/ping"
+
+      last_response.status.must_equal 412
+      last_response.body.must_equal last_build.sha
+    end
   end
 
-  def test_ping_building
-    current_build = build :building
-    CIJoe.any_instance.stubs(:current_build).returns(current_build)
+  describe 'POST /' do
+    it 'works with json' do
+      CIJoe.any_instance.expects(:build)
 
-    get "/ping"
-    assert_equal 412, last_response.status
-    assert_equal "building", last_response.body
+      post '/', :payload => File.read("#{Dir.pwd}/test/fixtures/payload.json")
+
+      last_response.status.must_equal 302
+    end
+
+    it 'does not build on branch mismatch' do
+      CIJoe.any_instance.expects(:build).never
+
+      post "/", :payload => {"ref" => "refs/heads/dont_build"}.to_json
+
+    end
+
+    it 'builds specific branch when specified' do
+      CIJoe.any_instance.expects(:build).with("branchname")
+
+      post "/?branch=branchname", :payload => {"ref" => "refs/heads/master"}.to_json
+
+      last_response.status.must_equal 302
+    end
+
+    it 'builds branch on match' do
+      CIJoe.any_instance.expects(:build)
+
+      post "/", :payload => {"ref" => "refs/heads/master"}.to_json
+
+      last_response.status.must_equal 302
+    end
+
+    it 'builds when the button its used' do
+      CIJoe.any_instance.expects(:build)
+
+      post "/", :rebuild => true
+      last_response.status.must_equal 302
+    end
   end
 
-  def test_ping_building_with_a_previous_build
-    last_build = build :worked
-    CIJoe.any_instance.stubs(:last_build).returns(last_build)
+  describe 'GET /api/json' do
+    it 'returns plain json without params' do
+      last_build = build :failed
+      CIJoe.any_instance.stubs(:last_build).returns(last_build)
 
-    current_build = build :building
-    CIJoe.any_instance.stubs(:current_build).returns(current_build)
-    CIJoe.any_instance.stubs(:building?).returns(true)
+      get "/api/json"
 
-    get "/ping"
-    assert_equal 412, last_response.status
-    assert_equal "building", last_response.body
+      last_response.status.must_equal 200
+      last_response.content_type.must_equal 'application/json'
+    end
+
+    it 'returns json with param' do
+      last_build = build :failed
+      CIJoe.any_instance.stubs(:last_build).returns(last_build)
+
+      get "/api/json?jsonp=fooberz"
+
+      last_response.status.must_equal 200
+      last_response.content_type.must_equal 'application/json'
+      last_response.body.must_match /^fooberz\(/
+    end
   end
-
-  def test_ping_failed
-    last_build = build :failed
-    CIJoe.any_instance.stubs(:last_build).returns(last_build)
-
-    get "/ping"
-    assert_equal 412, last_response.status
-    assert_equal last_build.sha, last_response.body
-  end
-
-  def test_post_with_json_works
-    CIJoe.any_instance.expects(:build)
-
-    post '/', :payload => File.read("#{Dir.pwd}/test/fixtures/payload.json")
-    assert_equal 302, last_response.status
-  end
-
-  def test_post_does_not_build_on_branch_mismatch
-    CIJoe.any_instance.expects(:build).never
-
-    post "/", :payload => {"ref" => "refs/heads/dont_build"}.to_json
-
-    assert_equal 302, last_response.status
-  end
-
-  def test_post_builds_specific_branch
-    CIJoe.any_instance.expects(:build).with("branchname")
-
-    post "/?branch=branchname", :payload => {"ref" => "refs/heads/master"}.to_json
-    assert_equal 302, last_response.status
-  end
-
-  def test_post_does_build_on_branch_match
-    CIJoe.any_instance.expects(:build)
-
-    post "/", :payload => {"ref" => "refs/heads/master"}.to_json
-    assert_equal 302, last_response.status
-  end
-
-  def test_post_does_build_when_build_button_is_used
-    CIJoe.any_instance.expects(:build)
-
-    post "/", :rebuild => true
-    assert_equal 302, last_response.status
-  end
-
-  def test_jsonp_should_return_plain_json_without_param
-    last_build = build :failed
-    CIJoe.any_instance.stubs(:last_build).returns(last_build)
-
-    get "/api/json"
-    assert_equal 200, last_response.status
-    assert_equal 'application/json', last_response.content_type
-  end
-
-  def test_jsonp_should_return_jsonp_with_param
-    last_build = build :failed
-    CIJoe.any_instance.stubs(:last_build).returns(last_build)
-
-    get "/api/json?jsonp=fooberz"
-    assert_equal 200, last_response.status
-    assert_equal 'application/json', last_response.content_type
-    assert_match /^fooberz\(/, last_response.body
-  end
-
-  def test_should_not_barf_when_no_build
-  end
-
-  # Create a new, fake build. All we care about is status.
 
   def build status
     CIJoe::Build.new_from_hash(
       {
-      :project_path => 'path',
-      :user         => 'user',
-      :project      => 'project',
-      :started_at   => Time.now,
-      :finished_at  => Time.now,
-      :sha          => 'deadbeef',
-      :status       => status,
-      :output       => 'output',
-      :pid          => nil
-    })
+        :project_path => 'path',
+        :user         => 'user',
+        :project      => 'project',
+        :started_at   => Time.now,
+        :finished_at  => Time.now,
+        :sha          => 'deadbeef',
+        :status       => status,
+        :output       => 'output',
+        :pid          => nil
+      })
   end
 end
