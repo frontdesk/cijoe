8baf1254f91ee37afb076e15257483ae55fece45
Andrew Coldham <quasor@me.com>
2013-11-14 15:53:12 -0800

    don't double dequeue

--- lib/cijoe.rb
+++ lib/cijoe.rb
@@ -201,8 +201,9 @@ EOS
   def restore
     @last_build = read_build('last')
 
+    # Check if process is still running
     Process.kill(0, @current_build.pid) if @current_build && @current_build.pid
-    build(@queue.next_branch_to_build) if !building? && @queue.waiting?
+    build if !building? && @queue.waiting?
   rescue Errno::ESRCH
     # build pid isn't running anymore. assume previous
     # server died and reset.
